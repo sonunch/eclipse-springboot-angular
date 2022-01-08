127fefa54266823885fd0071a0b1719b98bafc69
CSY <sonunch@hanmail.net>
2022-01-08 20:33:15 +0900

    spring bulid

---
+++ example-spring/src/main/java/kr/co/yunjava/configuration/DatabaseConfiguration.java
@@ -0,0 +1,16 @@
+package kr.co.yunjava.configuration;
+
+import javax.sql.DataSource;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+
+public class DatabaseConfiguration {
+
+	@Bean
+	@ConfigurationProperties(prefix="spring.datasource")
+	public DataSource dataSource(){
+		return DataSourceBuilder.create().build();
+	}
+}
